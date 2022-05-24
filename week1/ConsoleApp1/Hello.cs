0e18a23d8b08a08f53fa9112cf7ef468cdeef308
HQyang <70936623+HyunQyang@users.noreply.github.com>
2022-05-24 23:55:03 +0900

    Add files via upload

---
+++ week1/ConsoleApp1/Hello.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Hello
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("안녕하세요 C# 프로그래밍");
+        }
+    }
+}
