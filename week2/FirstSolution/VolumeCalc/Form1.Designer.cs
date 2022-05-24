bf44d0bc1d437fb410f64c90e9f2757ec9728e38
HQyang <70936623+HyunQyang@users.noreply.github.com>
2022-05-24 23:55:35 +0900

    Add files via upload

---
+++ week2/FirstSolution/VolumeCalc/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿
+namespace VolumeCalc
+{
+    partial class frmVolumeCalc
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Fire = new System.Windows.Forms.TextBox();
+            this.btnCalc = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtInt = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // Fire
+            // 
+            this.Fire.Location = new System.Drawing.Point(178, 229);
+            this.Fire.Multiline = true;
+            this.Fire.Name = "Fire";
+            this.Fire.Size = new System.Drawing.Size(207, 28);
+            this.Fire.TabIndex = 2;
+            this.Fire.TextChanged += new System.EventHandler(this.Fire_TextChanged);
+            // 
+            // btnCalc
+            // 
+            this.btnCalc.Location = new System.Drawing.Point(234, 178);
+            this.btnCalc.Name = "btnCalc";
+            this.btnCalc.Size = new System.Drawing.Size(75, 23);
+            this.btnCalc.TabIndex = 4;
+            this.btnCalc.Text = "계산하기";
+            this.btnCalc.UseVisualStyleBackColor = true;
+            this.btnCalc.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(105, 101);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(17, 12);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "월";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(105, 230);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(17, 12);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "일";
+            // 
+            // txtInt
+            // 
+            this.txtInt.Location = new System.Drawing.Point(189, 92);
+            this.txtInt.Multiline = true;
+            this.txtInt.Name = "txtInt";
+            this.txtInt.Size = new System.Drawing.Size(60, 21);
+            this.txtInt.TabIndex = 7;
+            // 
+            // frmVolumeCalc
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(550, 419);
+            this.Controls.Add(this.txtInt);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnCalc);
+            this.Controls.Add(this.Fire);
+            this.Name = "frmVolumeCalc";
+            this.Text = "월";
+            this.Load += new System.EventHandler(this.frmVolumeCalc_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox Fire;
+        private System.Windows.Forms.Button btnCalc;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtInt;
+    }
+}
+
---
+++ week2/FirstSolution/VolumeCalc/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VolumeCalc
+{
+    public partial class frmVolumeCalc : Form
+    {
+        public frmVolumeCalc()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int a;
+            int b = 0;
+            a = int.Parse(txtInt.Text);
+
+            switch (a)
+            {
+                case 1: 
+                case 3:
+                case 5:
+                case 7:
+                case 8:
+                case 10:
+                case 12: b = 31; break;
+
+                case 2:
+                case 4:
+                case 6:
+                case 9:
+                case 11: b = 30; break;
+     
+            }
+
+            Fire.Text = b + "";
+
+        }
+
+        private void frmVolumeCalc_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Fire_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
